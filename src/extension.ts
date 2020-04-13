a4f51ef95d133e31ce82960c091a96a43fef7634
bfitzpat@redhat.com <bfitzpat@redhat.com>
2020-04-13 16:53:22 -0600

    added extension- and workspace-provided TOC examples
    Signed-off-by: bfitzpat@redhat.com <bfitzpat@redhat.com>

--- src/extension.ts
+++ src/extension.ts
@@ -12,6 +12,7 @@ export async function activate(context: vscode.ExtensionContext) {
 	console.log('Congratulations, your extension "didact-ext" is now active!');
 
 	await registerTutorialWithDidact(context);
+	await registerTOCExampleWithDidact(context);
 }
 
 // this method is called when your extension is deactivated
@@ -26,6 +27,31 @@ async function registerTutorialWithDidact(context: vscode.ExtensionContext) {
 			const commandId = 'vscode.didact.register';
 			const tutorialName = `Tutorial-from-extension`; // replace this with the name of your integration to appear in the Didact view
 			const tutorialPath = path.join(context.extensionPath, `./tutorial/hosted.didact.md`); // replace this path with the location of your Didact file like ./path/to/tutorial.didact.md or ./path/to/tutorial.didact.adoc
+			const tutorialUri = vscode.Uri.parse(`file://${tutorialPath}`);
+			const tutorialCategory = `didact-ext`; // replace this with the name of the category for your tutorial
+
+			// then pass name, uri, and category
+			await vscode.commands.executeCommand(
+				commandId,
+				tutorialName, 
+				tutorialUri,
+				tutorialCategory);
+		}
+	} catch (error) {
+		console.log(error);
+	}
+
+}
+
+async function registerTOCExampleWithDidact(context: vscode.ExtensionContext) {
+	try {
+		// test to ensure didact is available 
+		const extensionId = 'redhat.vscode-didact';
+		const didactExt : any = vscode.extensions.getExtension(extensionId);
+		if (didactExt) {
+			const commandId = 'vscode.didact.register';
+			const tutorialName = `Example-from-extension`; // replace this with the name of your integration to appear in the Didact view
+			const tutorialPath = path.join(context.extensionPath, `./tableofcontents/root-toc.didact.md`); // replace this path with the location of your Didact file like ./path/to/tutorial.didact.md or ./path/to/tutorial.didact.adoc
 			const tutorialUri = vscode.Uri.parse(`file://${tutorialPath}`);
 			const tutorialCategory = `didact-ext`; // replace this with the name of the category for your tutorial
 
